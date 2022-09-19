23ae00f09d89f70ee11e5cfb3dbab064cc8b5fbc
pranav <pranav_a@ece.iitr.ac.in>
2022-09-19 22:25:39 +0530

    intial configration

---
+++ portfolio.js
@@ -0,0 +1,249 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Pranav Arya",
+	title: "Hi all, I'm Pranav",
+	description:
+		"I'm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Next.js  and some other cool libraries and frameworks. I also work on Machine Learning and Deep Learning",
+	resumeLink:
+		"https://docs.google.com/document/d/1_4RIfbyt4FFHW_bNplnoINQr01neTu7XDeeUlT_2O_k/edit?usp=sharing",
+};
+
+export const openSource = {
+	githubUserName: "pranav-iitr",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	url: "https://google.com",
+	linkedin: "https://www.linkedin.com/in/pranav-arya-2673421b8/",
+	github: "https://github.com/pranav-iitr",
+	
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	data: [
+		{
+			title: "Full Stack Development",
+			lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji(
+					"⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js"
+				),
+				emoji("⚡ Building responsive static websites using Next.js"),
+				emoji(
+					"⚡ Building RESTful APIs in Django & Django REST Framework"
+				),
+			],
+			softwareSkills: [
+				{
+					skillName: "HTML-5",
+					fontAwesomeClassname: "vscode-icons:file-type-html",
+				},
+				{
+					skillName: "CSS-3",
+					fontAwesomeClassname: "vscode-icons:file-type-css",
+				},
+				{
+					skillName: "JavaScript",
+					fontAwesomeClassname: "logos:javascript",
+				},
+				{
+					skillName: "Reactjs",
+					fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+				},
+				{
+					skillName: "Nextjs",
+					fontAwesomeClassname: "vscode-icons:file-type-light-next",
+				},
+				{
+					skillName: "Python",
+					fontAwesomeClassname: "logos:python",
+				},
+				{
+					skillName: "Django",
+					fontAwesomeClassname: "vscode-icons:file-type-django",
+				},
+
+				{
+					skillName: "Redux",
+					fontAwesomeClassname: "logos:redux",
+				},
+				{
+					skillName: "NPM",
+					fontAwesomeClassname: "logos:npm-icon",
+				},
+				{
+					skillName: "Yarn",
+					fontAwesomeClassname: "logos:yarn",
+				},
+			],
+		},
+		
+		{
+			title: "ML",
+			lottieAnimationFile: "/lottie/skills/ai.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji(
+					"⚡ Learning New Machine Learning Algorithms"
+				),
+				emoji(
+					"⚡ Deploying Machine Learning Models to make world a better place"
+				),
+				emoji(
+					"⚡ Won First Prize in Inter IIT Tech Meet 10 in ML"
+				),
+				emoji(
+					"⚡ Intragrating Machine Learning and Deep Learning Models with Web Applications"
+				),
+			],
+			softwareSkills: [
+				{
+					skillName: "Python",
+					fontAwesomeClassname: "logos:python",
+				},
+				{
+					skillName: "Jupyter",
+					fontAwesomeClassname: "logos:jupyter",
+				},
+				{
+					skillName: "Web3js",
+					fontAwesomeClassname: "logos:web3js",
+				},
+				{
+					skillName: "Metamask",
+					fontAwesomeClassname: "logos:metamask-icon",
+				},
+				{
+					skillName: "Ganache",
+					fontAwesomeClassname: "logos:ganache-icon",
+				},
+			],
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Harvard University",
+		subHeader: "Master of Science in Computer Science",
+		duration: "September 2017 - April 2019",
+		desc: "Participated in the research of XXX and published 3 papers.",
+		grade: "Grade A",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+];
+
+export const experience = [
+	{
+		role: "Frontend Developer",
+		company: "Duseca Software",
+		companylogo: "/img/icons/common/dusecaSoftware.jpg",
+		date: "Apr 2022 – Jun 2022",
+		desc: "I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries.",
+	},
+	{
+		role: "API Engineer",
+		company: "Duseca Software",
+		companylogo: "/img/icons/common/dusecaSoftware.jpg",
+		date: "Jan 2022 – Mar 2022",
+		desc: "I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		role: "Full Stack Developer",
+		company: "Bleed-AI",
+		companylogo: "/img/icons/common/bleedAI.jpg",
+		date: "Sept 2021 - Oct 2021",
+		desc: "Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.",
+	},
+	{
+		role: "Backend Developer",
+		company: "Wapidu",
+		companylogo: "/img/icons/common/wapidu.jpg",
+		date: "Sept 2021",
+		desc: "Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.",
+	},
+];
+
+export const projects = [
+	{
+		name: "developer-portfolio",
+		desc: "Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.",
+		github: "https://github.com/1hanzla100/developer-portfolio",
+		link: "https://developer-portfolio-1hanzla100.vercel.app/",
+	},
+	{
+		name: "AtlasMart",
+		desc: "With Atlas Mart, it’s easy to find the products with the best price and ship them straight to your door.",
+		github: "https://github.com/1hanzla100/Django-React-Marketplace",
+	},
+	{
+		name: "Technota (Forum)",
+		desc: "Get hands-on experience in technical skills with Technota",
+		github: "https://github.com/1hanzla100/django-react-forum",
+	},
+	{
+		name: "Shopaza (Ecommerce)",
+		desc: "Now your reach to all your favorite entertainment and luxury items is just one touch apart",
+		github: "https://github.com/1hanzla100/Django-ecommerce",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "John Smith",
+		feedback:
+			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
+	},
+	{
+		name: "John Smith",
+		feedback:
+			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
+	},
+];
+
+// See object prototype on SEO.jsx page
+export const seoData = {
+	title: "PranavArya",
+	description:
+		"A passionate Full Stack Web Developer and Machine Learning enginner.",
+	author: "Pranav Arya",
+	image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+	
+	keywords: [
+		"Pranav Arya",
+		"Pranav ",
+		"Portfolio",
+		"ecell",
+		"e-cell",
+		"ecell IIT Roorkee",
+		"e-cell IIT Roorkee",
+	],
+
+}
